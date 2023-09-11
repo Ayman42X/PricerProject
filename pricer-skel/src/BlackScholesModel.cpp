c3ef64a87c55b7ce0ef726242940f6b8cf67111d
aymaen <mohamed-aymane.akil@grenoble-inp.org>
2023-09-11 12:27:34 +0200

    Price 0

--- pricer-skel/src/BlackScholesModel.cpp
+++ pricer-skel/src/BlackScholesModel.cpp
@@ -1,5 +1,6 @@
 #include "BlackScholesModel.hpp"
 #include <cmath>
+#include <iostream>
 
 BlackScholesModel::BlackScholesModel(int size, double r, double rho,PnlVect* sigma,PnlVect* spot){
     this->size_ = size;
@@ -12,26 +13,25 @@ BlackScholesModel::BlackScholesModel(int size, double r, double rho,PnlVect* sig
 void BlackScholesModel::asset(PnlMat* path, double T, int nbTimeSteps, PnlRng* rng){
     double timeStep = T/nbTimeSteps;
     int d = this->spot_->size;
-    path = pnl_mat_create(nbTimeSteps+1,d);
     pnl_mat_set_col(path,this->spot_,0);
     PnlVect* G = pnl_vect_new();
     PnlVect* L = pnl_vect_new();
-    PnlMat* matriceCorrelation = pnl_mat_create_from_scalar(nbTimeSteps+1,d,this->rho_);
-    for (int k = 0;k<nbTimeSteps+1;k++){
+    PnlMat* matriceCorrelation = pnl_mat_create_from_scalar(d,d,this->rho_);
+    for (int k = 0;k<d;k++){
         pnl_mat_set(matriceCorrelation,k,k,1.0);
     }
     pnl_mat_chol(matriceCorrelation);
     for (int t=1;t<nbTimeSteps+1;t++){
-        for (int j=1;j<d;j++){
+        for (int j=0;j<d;j++){
             double sigmaShare = pnl_vect_get(this->sigma_,j);
             double quantity = (this->r_ - (sigmaShare*sigmaShare)/2)*timeStep;
             pnl_vect_rng_normal(G,d,rng);
             pnl_mat_get_row(L,matriceCorrelation,j);
             quantity += sigmaShare*sqrt(timeStep)*pnl_vect_scalar_prod(G,L);
             quantity = exp(quantity);
-            double share = pnl_mat_get(path,t-1,j);
+            double share = pnl_mat_get(path,j,t-1);
             share*=quantity;
-            pnl_mat_set(path,t,j,share);
+            pnl_mat_set(path,j,t,share);
         } 
     }
     pnl_vect_free(&G);
@@ -45,13 +45,14 @@ void BlackScholesModel::asset(PnlMat* path, double t, double T, int nbTimeSteps,
     path = pnl_mat_create(nbTimeSteps+1,d);
     pnl_mat_set_subblock(path,past,0,0);
     PnlVect* G = pnl_vect_new();
+    pnl_vect_rng_normal(G,1,rng);
     PnlVect* L = pnl_vect_new();
     PnlMat* matriceCorrelation = pnl_mat_create_from_scalar(nbTimeSteps+1,d,this->rho_);
     for (int k = 0;k<nbTimeSteps+1;k++){
         pnl_mat_set(matriceCorrelation,k,k,1.0);
     }
     pnl_mat_chol(matriceCorrelation);
-    for (int t=1;t<nbTimeSteps+1;t++){
+    for (int temps=1;temps<nbTimeSteps+1;temps++){
         for (int j=1;j<d;j++){
             double sigmaShare = pnl_vect_get(this->sigma_,j);
             double quantity = (this->r_ - (sigmaShare*sigmaShare)/2)*timeStep;
---
+++ pricer-skel/src/TestBlackScholes.cpp
@@ -0,0 +1,14 @@
+#include "BlackScholesModel.hpp"
+#include "pnl/pnl_vector.h"
+#include <iostream>
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    PnlVect* sigma = pnl_vect_create_from_scalar(2,0.2);
+    PnlVect* spot = pnl_vect_create_from_scalar(2,100);
+    PnlMat* path = pnl_mat_create(2,11);
+    PnlRng* rng = pnl_rng_create(PNL_RNG_MERSENNE);
+    pnl_rng_sseed(rng, time(NULL));
+    BlackScholesModel* blackScholesModel = new BlackScholesModel(2,0.04,0.0,sigma,spot);
+    blackScholesModel->asset(path,5,10,rng);
+return 0;
+}
